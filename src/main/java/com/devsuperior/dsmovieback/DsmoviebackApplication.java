8b9bb997a38aaac790c6d6b6b7afdce720306a3f
fcandiotti <fernando_candiotti@hotmail.com>
2022-01-12 10:13:02 -0300

    git commit -m "Iniciando o Projeto"

---
+++ src/main/java/com/devsuperior/dsmovieback/DsmoviebackApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviebackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviebackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsmovieback/DsmoviebackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviebackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
